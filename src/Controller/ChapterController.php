e1a14fd8ab68289b79d452d737d09f9bd2c767c1
Alexandre <106594791+EauTarie@users.noreply.github.com>
2023-11-29 15:15:33 +0100

    Add: readme and update chapter route (new + edit + delete)

--- src/Controller/ChapterController.php
+++ src/Controller/ChapterController.php
@@ -8,11 +8,29 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ChapterController extends AbstractController
 {
-    #[Route('/chapter', name: 'app_chapter')]
+    #[Route('/chapter/list', name: 'app_chapter')]
     public function index(): Response
     {
         return $this->render('chapter/index.html.twig', [
             'controller_name' => 'ChapterController',
         ]);
+    }
+
+    #[Route('/chapter/new', methods: ['POST'], name: "new_chapter")]
+    public function new(): Response
+    {
+
+    }
+
+    #[Route('/chapter/edit/{id}', methods: ['PUT'], name: "edit_chapter")]
+    public function edit(int $id): Response
+    {
+
+    }
+
+    #[Route('/chapter/delete/{id}', methods: ['DELETE'], name: "delete_chapter")]
+    public function delete(int $id): Response
+    {
+
     }
 }
