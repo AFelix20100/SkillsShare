78545bd92d0e5831d962a309f24030acc4974fe7
ARTHUR Félix <arthur.felix160@gmail.com>
2023-11-30 11:42:34 +0100

    contact + about

--- src/Controller/CoursesController.php
+++ src/Controller/CoursesController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Courses;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CoursesController extends AbstractController
 {
     #[Route('/courses', name: 'app_courses')]
-    public function ShowCourses(): Response
+    public function showCourses(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('courses/index.html.twig', [
-            'controller_name' => 'CoursesController',
+        $courses = $entityManager->getRepository(Courses::class)->findAll();
+        return $this->render('courses/index.html.twig', 
+        [
         ]);
     }
     #[Route('/categories', name: 'app_categories')]
