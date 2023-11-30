6f266b4585efbaf615cb31b54db8ea002ba80489
Alexandre <106594791+EauTarie@users.noreply.github.com>
2023-11-30 12:13:49 +0100

    Add form to template, and selector courses to chapter form

--- src/Form/ChapterFormType.php
+++ src/Form/ChapterFormType.php
@@ -2,19 +2,29 @@
 
 namespace App\Form;
 
+use App\Entity\Courses;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
 
 class ChapterFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class)
-            ->add('Video_Url', TextType::class)
-            ->add('content', TextType::class)
+            ->add('title', TextType::class, ['label' => 'Titre'])
+            ->add('Video_Url', TextType::class, ['label' => 'Video URL'])
+            ->add('content', TextType::class, ['label' => 'Contenu'])
+            ->add('courses', EntityType::class, 
+            [
+                'class' => Courses::class,
+
+                // uses the User.username property as the visible option string
+                'choice_label' => 'chapter',
+            ])
         ;
     }
 
